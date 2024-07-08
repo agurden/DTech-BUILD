5bbdd36f76260141f6b75566cb481126d79af600
Abigail Gurden <abigail.gurden@duke.edu>
2024-07-08 19:37:19 -0400

    user added

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+public class User {
+    private String userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private int age;
+    private String gender;
+
+    public User(){}
+
+    public User(String firstName, String lastName, String email, String password, int age, String gender) {
+
+        this.userID = generateUID(firstName, lastName);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    private String generateID(String firstName, String lastName){
+        return "" + firstName.charAt(0) + lastName.charAt(0) + Math.random()*100;
+    }
+
+    private boolean uniqueID(String id){
+        //check if unique
+        return true;
+    }
+
+    private String generateUID(String firstName, String lastName){
+        String id = generateID(firstName, lastName);
+        while(!uniqueID(id)){
+            id = generateID(firstName, lastName);
+        }
+        return id;
+    }
+}
